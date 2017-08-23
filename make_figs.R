4f345ed5b140ef50375c138f78d712759c5ab5d8
Alexander Holyoake <ajholyoake@gmail.com>
2017-08-23 22:47:51 +0100

    data tidy

--- make_figs.R
+++ make_figs.R
@@ -2,11 +2,11 @@ setwd('~/courses/bayes/project')
 
 load.all.data <- function() {
   
-  weight.head <- read.csv('data/dressed_weight_per_head.csv')
-  home.per.month <- read.delim('data/slaug_cont_mth_17sep15_home.csv',comment.char = '#')
-  month <- read.csv('data/slaughter_mth_17sep15.csv')
-  week <- read.csv('data/slaughter_wk_17sep15.csv')
-  supplies <- read.csv('data/slgter_supplies_26jun15.csv')
+  weight.head <- read.csv('data/dressed_weight_per_head.csv', comment.char = '#')
+  home.per.month <- read.csv('data/slaug_cont_mth_17sep15_home.csv',comment.char = '#')
+  month <- read.csv('data/slaughter_mth_17sep15.csv', comment.char = '#')
+  week <- read.csv('data/slaughter_wk_17sep15.csv', comment.char = '#')
+  supplies <- read.csv('data/slgter_supplies_26jun15.csv', comment.char = '#')
 
   return ( list(weight.head=weight.head,
                 home.per.month = home.per.month,
